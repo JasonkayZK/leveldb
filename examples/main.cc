4ca51b53cfcba9e890ec75dca5aef3dd2a56db59
JasonkayZK <271226192@qq.com>
2022-12-09 13:39:33 +0800

    add demo

--- examples/main.cc
+++ examples/main.cc
@@ -1,11 +1,23 @@
 #include <cassert>
+#include <iostream>
 
 #include "leveldb/db.h"
 
-int main() {
+#include "gtest/gtest.h"
+
+TEST(Usage, InitDb) {
   leveldb::DB* db;
   leveldb::Options options;
   options.create_if_missing = true;
-  leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+  leveldb::Status status = leveldb::DB::Open(options, "/tmp/test_db", &db);
+
+  std::cout << "db started, status: " << status.ToString() << std::endl;
   assert(status.ok());
+  delete db;
+}
+
+int main(int argc, char** argv) {
+  printf("Running main() from %s\n", __FILE__);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
